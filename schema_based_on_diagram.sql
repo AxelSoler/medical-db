9ae325e4ec9ddcab229bc4d4eaad4ce5f4a111bb
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-07-12 15:38:50 -0300

    table invoice_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,8 +5,17 @@ CREATE TABLE patients (
 );
 
 CREATE TABLE treatments (
-    id INT SERIAL,
+    id SERIAL,
     type VARCHAR(50),
     name VARCHAR(50),
     PRIMARY KEY (id)
+);
+
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY,
+    unit_price decimal,
+    quantity integer,
+    total_price decimal,
+    invoice_id integer,
+    treatment_id integer
 );
