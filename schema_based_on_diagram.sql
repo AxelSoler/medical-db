2abeb255d292e76d73936d8c912de60d3bfbabc1
marurevi <maryrev@hotmail.com>
2022-07-12 15:31:22 -0300

    Add Treatments table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,5 +1,12 @@
 CREATE TABLE patients (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
+    name VARCHAR(255),
     date_of_birth DATE
-)
+);
+
+CREATE TABLE treatments (
+    id INT SERIAL,
+    type VARCHAR(50),
+    name VARCHAR(50),
+    PRIMARY KEY (id)
+);
